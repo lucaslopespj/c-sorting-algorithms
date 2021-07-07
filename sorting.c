433d001e30c7cace6dd41215403e7b0fdf467f32
Lucas Lopes <lucas.lopes.paula@usp.br>
2021-07-07 15:35:30 -0300

    Insertion Sort algorithm has been implemented

--- sorting.c
+++ sorting.c
@@ -1,1 +1,32 @@
-/* A file to write sorting algorithms */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Insertion Sort Algorithm */
+void insertionSort(int *v, int n) {
+    int i, j, x;
+    
+    for (i = 1; i < n; i++) {
+        x = v[i];
+        for (j = i; (j > 0) && (x < v[j-1]); j--)
+            v[j] = v[j-1];
+        v[j] = x;
+    }
+}
+
+int main() {
+    int v[6] = {5, 3, -1, 0, 4, 10};
+    int i;
+
+    printf("Original vector: ");
+    for (i = 0; i < 6; i++)
+        printf("%d ", v[i]);
+
+    printf("\nSorted vector: ");
+    insertionSort(v, 6);
+
+    for (i = 0; i < 6; i++)
+        printf("%d ", v[i]);
+
+    printf("\n");
+    return 0;
+}
--- sorting.h
+++ sorting.h
@@ -1,1 +1,5 @@
 /* A file to write the header file (interface) */
+
+/* This function receives an array of integers of 
+size n and sorts it*/
+void insertionSort(int *v, int n);

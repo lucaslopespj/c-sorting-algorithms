3069b5cb6fc09dfe8fd81efacc1c0f89dd84d650
Lucas Lopes <lucas.lopes.paula@usp.br>
2021-07-09 00:33:17 -0300

    Function 'readNumFromFile' has been implemented

--- fileHandling.c
+++ fileHandling.c
@@ -2,18 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    /*int v[6] = {5, 3, -1, 0, 4, 10};*/
-    int tam = 15;
-    int v[15];
-    int num = 0;
-    int i = 0;
-    int neg = 0;
-    int digits = 1;
+int readNumFromFile(int **v, const char *fileName) {
+    int i = 0, t = 0, neg = 0, num = 0;
     char ch = 0;
     FILE *fs = NULL;
 
-    fs = fopen("input.txt", "r");
+    fs = fopen(fileName, "r");
 
     if (fs == NULL) {
         printf("Error on open the file\n");
@@ -33,7 +27,71 @@ int main() {
                     num = -num;
                     neg = 0;
                 }
+                t++;
+                *v = realloc(*v, t * sizeof(int));    
+                (*v)[i++] = num;
+                num = 0;
+                break;
+        
+            default:
+                if (ch != ' ' && ch != EOF) {
+                    num *= 10;
+                    num += atoi(&ch);
+                }
+                else if (ch == EOF) {
+                    (*v)[i] = num;
+                }
+        }
+
+        if (ch == EOF)
+            break;
+    }
+
+    fclose(fs);
+    return ++t;
+}
+
+int main() {
+    /*int v[6] = {5, 3, -1, 0, 4, 10};
+    int tam = 15;
+    int v[15];
+    int num = 0;
+    int t = 0;
+    int i = 0;
+    int neg = 0;
+    int digits = 1;
+    char ch = 0;
+    FILE *fs = NULL;
+    int *vet;
+
+    fs = fopen("input.txt", "r");
+
+    if (fs == NULL) {
+        printf("Error on open the file\n");
+        return -1;
+    }
+
+    vet = malloc(sizeof(int));
+
+    while (1) {
+        ch = getc(fs);
+        t++;
+        vet = realloc(vet, t * sizeof(int));
+
+        switch (ch) {
+            case '-':
+                neg = 1;
+                break;
+            
+            case ',':
+                if (neg) {
+                    num = -num;
+                    neg = 0;
+                }
+                t++;
+                vet = realloc(vet, (t+1) * sizeof(int));
                 v[i++] = num;
+                vet[i] = num;
                 num = 0;
                 break;
         
@@ -43,19 +101,27 @@ int main() {
                     num += atoi(&ch);
                 }
                 
-                if (ch == EOF)
+                else if (ch == EOF) {
                     v[i] = num;
+                    vet[i] = num;
+                }
         }
 
         if (ch == EOF)
             break;
     }
 
-    fclose(fs);
+    fclose(fs);*/
+    int *vet = NULL;
+    int t, i;
+    const char *file = "input.txt";
+    printf("%s\n", file);
+    t = readNumFromFile(&vet, file);
 
     printf("Original vector: ");
-    for (i = 0; i < tam; i++)
-        printf("%d ", v[i]);
+    for (i = 0; i < t; i++) {
+        printf("%d ", vet[i]);
+    }
 
     /*
     printf("\nSorted vector: ");
@@ -84,6 +150,10 @@ int main() {
 
 
     fclose(fs);*/
+
+    free(vet);
+    vet = NULL;
+    printf("\n\nt = %d\n", t);
 
     return 0;
 }
--- fileHandling.h
+++ fileHandling.h
@@ -1,1 +1,9 @@
 /* Code in this library is to help read and write data in files */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* It receives the address of an integer array 'v' and a 'fileName' 
+string to the file's path that contains the numbers to be read.
+It reads all numbers in the file pointed by the 'fileName' string,
+puts them all in 'v' and returns the new size of the array 'v'. */
+int readNumFromFile(int **v, const char *fileName);

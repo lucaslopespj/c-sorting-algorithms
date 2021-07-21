98f02f01fb0c5c8e3076d5b3f7f8b7798f7c56be
Lucas Lopes <lucas.lopes.paula@usp.br>
2021-07-21 00:25:47 -0300

    Computing time of each sort algorithm has been implemented

--- main.c
+++ main.c
@@ -2,65 +2,87 @@
 #include <stdlib.h>
 #include <math.h>
 #include "fileHandling.h"
+#include "sorting.h"
+#include <time.h>
 
 int main() {
-    /*int v[10] = {5, 3, -1, 0, 4, 10, -35, 150, 2, 7};
-    int tam = 10;*/
-    int i = 0;
-    int **v = malloc(sizeof(int *));
-    int tam = 0;
-    int vet[60000];
-    int *t = NULL;
+    int i, tam = 0;
+    int *v = NULL;
     const char *output = "output.txt";
-    /*int num = 10000000;*/
-    int num = 1000000;
-    *v = malloc(sizeof(int));
-    t = malloc(num * sizeof (int));
+    const char *input = "input.txt";
+    int num = 100000;
+    double t_insertion, t_selection, t_bubble, t_merge, t_quick;
+    clock_t before, after;
+
+    /* Alocates memory to a quantity of "num" numbers */
+    v = malloc(num * sizeof (int));
     
+    /* Get all the random numbers */
     for (i = 0; i < num; i++) {
-        t[i] = randNum(7);
+        v[i] = randNum(7);
     }
 
-    writeOnFile(t, num, "input.txt");
+    /* Writes all random numbers in the "input.txt" file */
+    writeOnFile(v, num, input);
 
-    free(t);
-    t = NULL;
-
-    tam = readNumFromFile(&t, "input.txt");
-
-    /*printf("Original vector: ");
-    for (i = 0; i < tam; i++)
-        printf("%d ", (*v)[i]);
-
-    printf("\nSorted vector: ");*/
-    /*insertionSort(t, tam);*/
-    /*selectionSort(v, tam);*/
-    /*bubbleSort(v, tam);*/
-    /*mergeSort(t, 0, tam-1);*/
-    quickSort(t, 0, tam-1);
-
-    /*for (i = 0; i < tam; i++) {
-        if (i < tam-1)
-            printf("%d, ", (*v)[i]);
-        else
-            printf("%d\n", (*v)[i]);
-    }       
-
-    for (i = 0; i < tam; i++)
-        vet[i] = (*v)[i];*/
-
-    /*for (i = 0; i < tam; i++)
-        printf("%d, ", vet[i]);
-    printf("\n"); */
-    
-    writeOnFile(t, tam, output);
-
-    free(*v);
+    /* Free all memory alocated to v */
     free(v);
-    free(t);
-    t = NULL;
-    *v = NULL;
     v = NULL;
+
+    /* Insertion Sort Computing Time */
+    tam = readNumFromFile(&v, input);
+    before = clock();
+    insertionSort(v, tam);
+    after = clock();
+    t_insertion = (double) (after - before) / CLOCKS_PER_SEC;
+
+    /* Selection Sort Computing Time */
+    tam = readNumFromFile(&v, input);
+    before = clock();
+    selectionSort(v, tam);
+    after = clock();
+    t_selection = (double) (after - before) / CLOCKS_PER_SEC;
+
+    /* Bubble Sort Computing Time */
+    tam = readNumFromFile(&v, input);
+    before = clock();
+    bubbleSort(v, tam);
+    after = clock();
+    t_bubble = (double) (after - before) / CLOCKS_PER_SEC;
+
+    /* Merge Sort Computing Time */
+    tam = readNumFromFile(&v, input);
+    before = clock();
+    mergeSort(v, 0, tam-1);
+    after = clock();
+    t_merge = (double) (after - before) / CLOCKS_PER_SEC;
+
+    /* Quick Sort Computing Time */
+    tam = readNumFromFile(&v, input);
+    before = clock();
+    quickSort(v, 0, tam-1);
+    after = clock();
+    t_quick = (double) (after - before) / CLOCKS_PER_SEC;
+
+    /* Writes all sorted numbers in the "output.txt" file*/
+    writeOnFile(v, tam, output);
+
+    /* Free all memory alocated to v */
+    free(v);
+    v = NULL;
+
+    printf("\n");
+    printf("+-------------------------- STATISTICS ---------------------------+\n\n");
+    printf("         You have sorted %d random numbers.\n", num);
+    printf("         The 'input.txt' file contains all random numbers.\n");
+    printf("         The 'output.txt' file contains all sorted numbers.\n");
+    printf("         Time of each algorithm (seconds):\n");
+    printf("             Insertion Sort: %f s\n", t_insertion);
+    printf("             Selection Sort: %f s\n", t_selection);
+    printf("             Bubble Sort: %f s\n", t_bubble);
+    printf("             Merge Sort: %f s\n", t_merge);
+    printf("             Quick Sort: %f s\n\n", t_quick);
+    printf("+-----------------------------------------------------------------+\n\n");
 
     return 0;
 }

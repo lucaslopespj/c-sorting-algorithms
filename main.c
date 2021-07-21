ddc45ea892853fd5fc8f208c367b39d23156f5a3
Lucas Lopes <lucas.lopes.paula@usp.br>
2021-07-21 12:37:05 -0300

    Some variables' names have been changed

--- main.c
+++ main.c
@@ -6,90 +6,90 @@
 #include <time.h>
 
 int main() {
-    int i, tam = 0;
+    int i, size = 0;
     int *v = NULL;
     const char *output = "output.txt";
     const char *input = "input.txt";
-    int num = 1000000;
+    int v_size = 100000;
     double t_insertion, t_selection, t_bubble, t_merge, t_quick;
     clock_t before, after;
 
-    /* Allocates memory to a quantity of "num" numbers */
-    v = malloc(num * sizeof (int));
+    /* Allocates memory to a quantity of "v_size" numbers */
+    v = malloc(v_size * sizeof (int));
     
     /* Get all the random numbers */
-    for (i = 0; i < num; i++) {
+    for (i = 0; i < v_size; i++) {
         v[i] = randNum(7);
     }
 
     /* Writes all random numbers in the "input.txt" file */
-    writeOnFile(v, num, input);
+    writeOnFile(v, v_size, input);
 
-    /* Free all memory alocated to v */
+    /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     /* Insertion Sort Computing Time */
-    tam = readNumFromFile(&v, input);
+    size = readNumFromFile(&v, input);
     before = clock();
-    insertionSort(v, tam);
+    insertionSort(v, size);
     after = clock();
     t_insertion = (double) (after - before) / CLOCKS_PER_SEC;
 
-    /* Free all memory alocated to v */
+    /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     /* Selection Sort Computing Time */
-    tam = readNumFromFile(&v, input);
+    size = readNumFromFile(&v, input);
     before = clock();
-    selectionSort(v, tam);
+    selectionSort(v, size);
     after = clock();
     t_selection = (double) (after - before) / CLOCKS_PER_SEC;
 
-    /* Free all memory alocated to v */
+    /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     /* Bubble Sort Computing Time */
-    tam = readNumFromFile(&v, input);
+    size = readNumFromFile(&v, input);
     before = clock();
-    bubbleSort(v, tam);
+    bubbleSort(v, size);
     after = clock();
     t_bubble = (double) (after - before) / CLOCKS_PER_SEC;
 
-    /* Free all memory alocated to v */
+    /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     /* Merge Sort Computing Time */
-    tam = readNumFromFile(&v, input);
+    size = readNumFromFile(&v, input);
     before = clock();
-    mergeSort(v, 0, tam-1);
+    mergeSort(v, 0, size-1);
     after = clock();
     t_merge = (double) (after - before) / CLOCKS_PER_SEC;
 
-    /* Free all memory alocated to v */
+    /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     /* Quick Sort Computing Time */
-    tam = readNumFromFile(&v, input);
+    size = readNumFromFile(&v, input);
     before = clock();
-    quickSort(v, 0, tam-1);
+    quickSort(v, 0, size-1);
     after = clock();
     t_quick = (double) (after - before) / CLOCKS_PER_SEC;
 
     /* Writes all sorted numbers in the "output.txt" file*/
-    writeOnFile(v, tam, output);
+    writeOnFile(v, size, output);
 
-    /* Free all memory alocated to v */
+    /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     printf("\n");
     printf("+-------------------------- STATISTICS ---------------------------+\n\n");
-    printf("         You have sorted %d random numbers.\n", num);
+    printf("         You have sorted %d random numbers.\n", v_size);
     printf("         The 'input.txt' file contains all random numbers.\n");
     printf("         The 'output.txt' file contains all sorted numbers.\n");
     printf("         Time of each algorithm (seconds):\n");

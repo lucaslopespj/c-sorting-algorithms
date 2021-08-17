0f66de5f8e34bc5bcbd96bf610da55083be71bec
Lucas Lopes <lucas.lopes.paula@usp.br>
2021-08-17 01:55:27 -0300

    New timing statistics were added to the main program

--- main.c
+++ main.c
@@ -15,9 +15,10 @@ int main(int argc, char const *argv[]) {
     int v_size = atoi(argv[1]);
     int num_quantity = atoi(argv[2]);
     
-    double t_insertion, t_selection, t_bubble, t_merge, t_quick, t_heap;
-    clock_t before, after;
-    tf *d_insertion, *d_selection, *d_bubble, *d_merge, *d_quick, *d_heap;
+    double aux_time = 0, total_time = 0, reading_time = 0;
+    clock_t before = 0, after = 0, totalBefore = 0, totalAfter = 0;
+    tf *tf_insertion, *tf_selection, *tf_bubble, *tf_merge, *tf_quick, *tf_heap;
+    tf *tf_total, *tf_input, *tf_output, *tf_random, *tf_reading;
 
     if (v_size <= 0) {
         printf("ERROR: You must choose a number greater than zero\n");
@@ -27,121 +28,215 @@ int main(int argc, char const *argv[]) {
     /* Allocates memory to a quantity of "v_size" numbers */
     v = malloc(v_size * sizeof (int));
     
-    /* Get all the random numbers */
+    before = clock();
+    /* Gets all the random numbers */
     for (i = 0; i < v_size; i++) {  
         v[i] = randNum(num_quantity);
     }
+    after = clock();
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    tf_random = timeInit(aux_time);
 
+    before = clock();
     /* Writes all random numbers in the "input.txt" file */
     writeOnFile(v, v_size, input);
+    after = clock();
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    tf_input = timeInit(aux_time);
 
     /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
-    /* Insertion Sort Computing Time */
+    /* Gets the number of clocks before all sorting algorithms */
+    totalBefore = clock();
+
+    /* Read file computation time */
+    before = clock();
     size = readNumFromFile(&v, input);
+    after = clock();
+    reading_time += (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+
+    /* Insertion Sort Computing Time */
     before = clock();
     insertionSort(v, size);
     after = clock();
-    t_insertion = (double) (after - before) / CLOCKS_PER_SEC;
-    d_insertion = timeInit(t_insertion);
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    total_time += aux_time;
+    tf_insertion = timeInit(aux_time);
 
     /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
-    /* Selection Sort Computing Time */
+    /* Read file computation time */
+    before = clock();
     size = readNumFromFile(&v, input);
+    after = clock();
+    reading_time += (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+
+    /* Selection Sort Computing Time */
     before = clock();
     selectionSort(v, size);
     after = clock();
-    t_selection = (double) (after - before) / CLOCKS_PER_SEC;
-    d_selection = timeInit(t_selection);
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    total_time += aux_time;
+    tf_selection = timeInit(aux_time);
 
     /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
-    /* Bubble Sort Computing Time */
+    /* Read file computation time */
+    before = clock();
     size = readNumFromFile(&v, input);
+    after = clock();
+    reading_time += (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+
+    /* Bubble Sort Computing Time */
     before = clock();
     bubbleSort(v, size);
     after = clock();
-    t_bubble = (double) (after - before) / CLOCKS_PER_SEC;
-    d_bubble = timeInit(t_bubble);
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    total_time += aux_time;
+    tf_bubble = timeInit(aux_time);
 
     /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
-    /* Merge Sort Computing Time */
+    /* Read file computation time */
+    before = clock();
     size = readNumFromFile(&v, input);
+    after = clock();
+    reading_time += (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+
+    /* Merge Sort Computing Time */
     before = clock();
     mergeSort(v, 0, size-1);
     after = clock();
-    t_merge = (double) (after - before) / CLOCKS_PER_SEC;
-    d_merge = timeInit(t_merge);
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    total_time += aux_time;
+    tf_merge = timeInit(aux_time);
 
     /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
-    /* Quick Sort Computing Time */
+    /* Read file computation time */
+    before = clock();
     size = readNumFromFile(&v, input);
+    after = clock();
+    reading_time += (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+
+    /* Quick Sort Computing Time */
     before = clock();
     quickSort(v, 0, size-1);
     after = clock();
-    t_quick = (double) (after - before) / CLOCKS_PER_SEC;
-    d_quick = timeInit(t_quick);
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    total_time += aux_time;
+    tf_quick = timeInit(aux_time);
+
+    /* Read file computation time */
+    before = clock();
+    size = readNumFromFile(&v, input);
+    after = clock();
+    reading_time += (double) (after - before) / CLOCKS_PER_SEC / 6;
+    tf_reading = timeInit(reading_time);
+    before = after = 0;
+
 
     /* Heap Sort Computing Time */
-    size = readNumFromFile(&v, input);
     before = clock();
     heapSort(v, size);
     after = clock();
-    t_heap = (double) (after - before) / CLOCKS_PER_SEC;
-    d_heap = timeInit(t_heap);
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    before = after = 0;
+    total_time += aux_time;
+    tf_heap = timeInit(aux_time);
     
+    /* Gets the number of clocks after all sorting algoritms */
+    totalAfter = clock();
+    aux_time = (double) (totalAfter - totalBefore) / CLOCKS_PER_SEC;
+    tf_total = timeInit(total_time);
 
+    before = clock();
     /* Writes all sorted numbers in the "output.txt" file*/
     writeOnFile(v, size, output);
+    after = clock();
+    aux_time = (double) (after - before) / CLOCKS_PER_SEC;
+    tf_output = timeInit(aux_time);
+
 
     /* Free all memory allocated to v */
     free(v);
     v = NULL;
 
     printf("\n");
-    printf("+-------------------------- STATISTICS ---------------------------+\n\n");
+    printf("+----------------------------------------- STATISTICS ------------------------------------------+\n\n");
     printf("         You have sorted %d random numbers.\n", v_size);
     printf("         The 'input.txt' file contains all random numbers.\n");
     printf("         The 'output.txt' file contains all sorted numbers.\n");
-    printf("         Time of each algorithm:\n");
+    printf("\n         Time of each algorithm:\n");
     
     printf("             Insertion Sort:");
-    printTime(d_insertion);
+    printTime(tf_insertion);
     
     printf("             Selection Sort:");
-    printTime(d_selection);
+    printTime(tf_selection);
 
     printf("             Bubble Sort:");
-    printTime(d_bubble);
+    printTime(tf_bubble);
 
     printf("             Merge Sort:");
-    printTime(d_merge);
+    printTime(tf_merge);
+    
     printf("             Quick Sort:");
-    printTime(d_quick);    
+    printTime(tf_quick);    
+    
     printf("             Heap Sort:");
-    printTime(d_heap);
+    printTime(tf_heap);
+    
+    printf("\n         Running time of all sorting algorithms :");
+    printTime(tf_total);
 
-    printf("+-----------------------------------------------------------------+\n\n");
+    printf("\n\n         Time to get the random numbers:");
+    printTime(tf_random);
 
-    timeKill(d_insertion);
-    timeKill(d_selection);
-    timeKill(d_bubble);
-    timeKill(d_merge);
-    timeKill(d_quick);
-    timeKill(d_heap);
+    printf("         Writing time of the 'input.txt' file:");
+    printTime(tf_input);
+    
+    printf("         Writing time of the 'output.txt' file:");
+    printTime(tf_output);
+
+    printf("         Mean reading time of the 'input.txt' file:");
+    printTime(tf_reading);
+
+
+    printf("\n+------------------------------------------------------------------------------------------------+\n\n");
+
+    timeKill(tf_insertion);
+    timeKill(tf_selection);
+    timeKill(tf_bubble);
+    timeKill(tf_merge);
+    timeKill(tf_quick);
+    timeKill(tf_heap);
+    timeKill(tf_random);
+    timeKill(tf_input);
+    timeKill(tf_output);
+    timeKill(tf_total);
 
     return 0;
 }
